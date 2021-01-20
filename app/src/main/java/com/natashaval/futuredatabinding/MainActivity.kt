e8b63d543db4f4b8e555f7ec4d437e2cd9573bf9
natashaval <natasha.valentina1998@gmail.com>
2021-01-20 15:31:58 +0700

    Init project

---
+++ app/src/main/java/com/natashaval/futuredatabinding/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.natashaval.futuredatabinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
